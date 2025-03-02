df8878b5d61e1d0b0db19736ffb2177a9e2d98eb
gao268 <1005092089@qq.com>
2025-03-02 16:11:22 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from argparse import ArgumentParser
-
+import inspect
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
